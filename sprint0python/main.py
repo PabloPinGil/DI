27b7fd6839782668ff042420447017e6048e6813
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2024-10-08 13:50:35 +0200

    Implementada clase main

---
+++ sprint0python/main.py
@@ -0,0 +1,9 @@
+def main():
+    nombre_heroe = input("Introduce el nombre de tu héroe: ")
+    heroe = Heroe(nombre_heroe)
+
+    mazmorra = Mazmorra(heroe)
+    mazmorra.jugar()
+
+if __name__ == '__main__':
+    main()
