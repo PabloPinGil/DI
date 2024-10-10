c8acc4dca4eff4a019d6c517dc97185ba4ed6fdd
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2024-10-10 18:58:13 +0200

    clase heroe completada

---
+++ sprint0python/Heroe.py
@@ -0,0 +1,36 @@
+class Heroe:
+    def __init__(self, nombre, ataque, defensa, salud, salud_maxima):
+        self.nombre = nombre
+        self.ataque = ataque
+        self.defensa = defensa
+        self.salud = salud
+        self.salud_maxima = salud_maxima
+
+    def atacar(self, enemigo):
+        print("El héroe ataca a " + enemigo.nombre)
+
+        if self.ataque > enemigo.defensa:
+            print("El enemigo " + enemigo.nombre + " ha recibido " + str(self.ataque) + "puntos de daño.")
+            enemigo.salud = enemigo.salud - self.ataque
+        else:
+            print("El enemigo ha bloqueado el ataque.")
+
+    def curarse(self):
+        self.salud = self.salud + 10
+        if self.salud > self.salud_maxima:
+            self.salud = self.salud_maxima
+
+        print("El héroe se ha curado. Salud actual: " + str(self.salud))
+
+    def defenderse(self):
+        self.defensa = self.defensa + 5
+        print("La defensa del héroe ha aumentado temporalmente a " + str(self.defensa))
+
+    def reset_defensa(self):
+        self.defensa = self.defensa - 5
+
+    def esta_vivo(self):
+        if self.salud > 0:
+            return True
+        else:
+            return False
