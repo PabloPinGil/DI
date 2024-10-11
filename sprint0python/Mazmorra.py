f2781150ca168d60127a7fa2f7f0710a166c6884
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2024-10-11 23:28:33 +0200

    clase mazmorra implementada

---
+++ sprint0python/Mazmorra.py
@@ -0,0 +1,66 @@
+from Monstruo import Monstruo
+from Tesoro import Tesoro
+
+
+class Mazmorra:
+    def __init__(self, heroe):
+        self.heroe = heroe
+        self.tesoro = Tesoro()
+        self.monstruos = []
+
+        for i in range(3):  # llena la lista con monstruos
+            nuevo_monstruo = Monstruo()
+            self.monstruos.append(nuevo_monstruo)
+
+    def jugar(self):    # método principal
+        ronda = 0
+        victoria = True
+
+        while ronda < len(self.monstruos):     # se ejecuta hasta matar a todos los enemigos o hasta que muera el héroe
+            print("El héroe entra en la mazmorra")
+            print("Te has encontrado con un " + self.monstruos[ronda].nombre)
+
+            while self.heroe.esta_vivo() and self.monstruos[ronda].esta_vivo():
+                self.enfrentar_enemigo(self.monstruos[ronda])
+
+            if not self.heroe.esta_vivo:
+                ronda = len(self.monstruos) + 1
+                victoria = False
+            else:
+                self.buscar_tesoro()
+
+            ronda += 1
+
+        if victoria:
+            print(f"¡{self.heroe.nombre} ha derrotado a todos los monstruos y ha conquistado la mazmorra!")
+        else:
+            print("El héroe ha sido derrotado en la mazmorra.")
+
+    def enfrentar_enemigo(self, enemigo):
+        fin = False
+
+        while not fin:
+            print("\n¿Que deseas hacer?"
+                  "\n1. Atacar"
+                  "\n2. Defender"
+                  "\n3. Curarse")
+            opcion = int(input("Elige una opción: "))
+
+            if opcion == 1:
+                self.heroe.atacar(enemigo)
+                fin = True
+
+            elif opcion == 2:
+                self.heroe.defenderse()
+                fin = True
+
+            elif opcion == 3:
+                self.heroe.curarse()
+                fin = True
+
+            else:
+                print("Opción no válida")
+
+    def buscar_tesoro(self):
+        print("Buscando tesoro...")
+        self.tesoro.encontrar_tesoro(self.heroe)
