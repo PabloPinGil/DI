4c58496be84da392a9339ece6850f06d808113e3
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2025-01-20 17:32:57 +0100

    implementada MainActivity con register y login

--- AppListaPeliculas/app/src/main/java/com/example/applistapeliculas/MainActivity.java
+++ AppListaPeliculas/app/src/main/java/com/example/applistapeliculas/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.applistapeliculas;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.registerButton).setOnClickListener(v -> registerUser());
+        findViewById(R.id.loginButton).setOnClickListener(v -> loginUser());
+    }
+
+    private void registerUser() {
+        String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Usuario registrado correctamente.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Error en el registro: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void loginUser() {
+        String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Error en autenticación.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
