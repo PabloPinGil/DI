7a2964e6f6d9647f43167d2ae855cba3d6bb6bf3
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2025-02-17 20:37:41 +0100

    semana 1: añadidos demás archivos del proyecto

--- AppListaPeliculas/app/build.gradle.kts
+++ AppListaPeliculas/app/build.gradle.kts
@@ -37,9 +37,19 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
