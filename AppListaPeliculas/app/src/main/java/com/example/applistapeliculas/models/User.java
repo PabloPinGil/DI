6d7943f7f72113b39bad0f86e108220bf53a78f2
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2025-02-18 01:58:37 +0100

    Semana 2: user model convertido a MVVM

---
+++ AppListaPeliculas/app/src/main/java/com/example/applistapeliculas/models/User.java
@@ -0,0 +1,59 @@
+package com.example.applistapeliculas.models;
+
+public class User {
+    private String uid;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    public User() {} // Required for Firebase
+
+    public User(String uid, String name, String email, String phone, String address) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
